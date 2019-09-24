6f398d910cccaa9439bdd9ebdba64140e33e6b7c
huang <huangchao20@126.com>
2019-09-24 16:10:52 +0800

    Initial commit

---
+++ Test_05/test01.py
@@ -0,0 +1,12 @@
+import matplotlib
+import matplotlib as mpl
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(123123)
+
+print(dir(matplotlib))
+print('------------------------------------------------------------------------')
+print('------------------------------------------------------------------------')
+print('------------------------------------------------------------------------')
+print(dir(matplotlib.pyplot))
